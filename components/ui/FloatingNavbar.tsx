ba3eeecf72e6f90ae6e9678f40d9cc393a376dc4
AndrewMTA <105147333+AndrewMTA@users.noreply.github.com>
2024-06-18 01:42:43 +0000

    menu

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -24,7 +24,7 @@ export const FloatingNav = ({
 
   // set true for the initial state so that nav bar is visible in the hero section
   const [visible, setVisible] = useState(true);
-
+const [close, setClose] = useState(true)
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     // Check if current is not undefined and is a number
     if (typeof current === "number") {
@@ -46,17 +46,7 @@ export const FloatingNav = ({
  return (
     <AnimatePresence mode="wait">
       <motion.div
-        initial={{
-          opacity: 1,
-          y: -100,
-        }}
-        animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
-        }}
-        transition={{
-          duration: 0.2,
-        }}
+     
         className={cn(
           "flex max-w-fit md:max-h-[80px] sm: max-h-[80px] md:min-w-[55vw] lg:min-w-[500px] sm: min-w-[250px] fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-between space-x-8",
           className
@@ -66,6 +56,7 @@ export const FloatingNav = ({
           backgroundColor: "rgba(17, 25, 40, 0.75)",
           borderRadius: "12px",
           border: "1px solid rgba(255, 255, 255, 0.125)",
+          position: 'absolute',
         }}
       >
         <div className="flex items-center space-x-4">
@@ -114,8 +105,27 @@ export const FloatingNav = ({
         </div>
         
              
-
-        <div  className="burger"><img style={{width: '30px', height: '30px'}} src="https://res.cloudinary.com/dojwag3u1/image/upload/v1718582369/menu_2_aznhfi.png"/> </div>
+{close ? <> 
+        <div  onClick={() => setClose(false)}className="burger"><img style={{width: '30px', height: '30px'}} src="https://res.cloudinary.com/dojwag3u1/image/upload/v1718582369/menu_2_aznhfi.png"/> </div>
+</> : <> 
+    <div className="dropdown-close" style={{background: '#fff' , opacity: '.8',  borderRadius: '50%'}}onClick={() => setClose(true)}>
+          x
+        </div>
+        <div className="menuBG"style={{position: 'absolute' , top: '70px', right: '30px', padding:'20px',  flexDirection: 'column'}}>
+        {navItems.map((navItem, idx) => (
+          <Link href={`${navItem.link}`} key={`link-${idx}`}>
+            <div
+              className={cn(
+                "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500 !cursor-pointer text-18px"
+              )}
+            >
+              <span className="block sm:hidden">{navItem.icon}</span>
+              <span style={{fontSize: '16px', marginTop: '5px'}} className="text-sm">{navItem.name}</span>
+            </div>
+          </Link>
+        ))}
+      </div></>}
+   
       </motion.div>
     </AnimatePresence>
   );
